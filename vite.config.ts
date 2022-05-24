e2d9698691793e6c55409433018929103e97527c
Jaime Aleman <jjaimealeman@gmail.com>
2022-05-24 02:04:41 -0600

    feat: add vite-plugin-vue-inspector.

--- vite.config.ts
+++ vite.config.ts
@@ -8,44 +8,54 @@ import pkg from "./package.json";
 import pages from "vite-plugin-pages";
 import layouts from "vite-plugin-vue-layouts";
 import components from "unplugin-vue-components/vite";
+import Inspector from "vite-plugin-vue-inspector";
 
 process.env.VITE_APP_VERSION = pkg.version;
 if (process.env.NODE_ENV === "production") {
-  process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString();
+    process.env.VITE_APP_BUILD_EPOCH = new Date().getTime().toString();
 }
 
 export default defineConfig({
-  plugins: [
-    vue({
-      script: {
-        refSugar: true,
-      },
-    }),
-    pages(),
-    layouts(),
+    plugins: [
+        vue({
+            script: {
+                refSugar: true,
+            },
+        }),
+        pages(),
+        layouts(),
+        Inspector({
+            enabled: true,
+            toggleComboKey: "control-shift"
+        }),
 
-    // https://github.com/antfu/unplugin-vue-components
-    components({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ["vue", "md"],
-      // allow auto import and register components used in markdown
-      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: "src/components.d.ts",
-    }),
+        // https://github.com/antfu/unplugin-vue-components
+        components({
+            // allow auto load markdown components under `./src/components/`
+            extensions: ["vue", "md"],
+            // allow auto import and register components used in markdown
+            include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+            dts: "src/components.d.ts",
+        }),
 
-    // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
-    vueI18n({
-      runtimeOnly: true,
-      compositionOnly: true,
-      include: [path.resolve(__dirname, "locales/**")],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+        // https://github.com/intlify/bundle-tools/tree/main/packages/vite-plugin-vue-i18n
+        vueI18n({
+            runtimeOnly: true,
+            compositionOnly: true,
+            include: [path.resolve(__dirname, "locales/**")],
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  test: {
-    include: ['tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
+    test: {
+        include: [
+            "tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}",
+        ],
+    },
+
+
+
 });
