5bd725d4abf3666c13bd6870308fe3a685815883
Jaime Aleman <jjaimealeman@gmail.com>
2022-05-24 02:12:54 -0600

    feat: add LanguageSwitcher.

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <header class="navbar bg-base-100">
-    <div class="flex-none">
+    <header class="navbar bg-base-100">
+        <div class="flex-none">
       <a class="btn btn-ghost text-xl normal-case">Company Name</a>
     </div>
-    <div class="flex-none">
+        <div class="flex-none">
       <ul class="menu menu-horizontal p-0">
         <li>
           <router-link to="/">Home</router-link>
@@ -17,16 +17,19 @@
         <li>
           <router-link to="/about">About</router-link>
         </li>
+        <li>
+            <LanguageSwitcher />
+        </li>
       </ul>
     </div>
-    <div class="flex-1">
+        <div class="flex-1">
       <slot></slot>
     </div>
-  </header>
+    </header>
 </template>
 
 <script setup lang="ts">
-import { useCounterStore } from '@/stores/counter'
+import { useCounterStore } from '@/stores/counter';
 
 const counter = useCounterStore()
 </script>
