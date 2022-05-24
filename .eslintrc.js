2ce58a00c9eafcb95d387600b8c175d6aaff07f7
Jaime Aleman <jjaimealeman@gmail.com>
2022-05-24 02:03:06 -0600

    feat: add fonts.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'comma-dangle': ['error', 'only-multiline'],
+    'vue/multi-word-component-names': 'off'
   },
   globals: {
     defineProps: 'readonly',
