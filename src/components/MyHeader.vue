8ddd3f36aceb6930f8c7902601a1791ac8636771
Jaime Aleman <jjaimealeman@gmail.com>
2022-05-27 11:52:00 -0600

    feat: myheader // white font.

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,30 +1,30 @@
 <template>
     <header class="navbar bg-base-100">
         <div class="flex-none">
-      <a class="btn btn-ghost text-xl normal-case">Company Name</a>
-    </div>
+            <a class="btn btn-ghost text-xl normal-case text-white">Company Name</a>
+        </div>
         <div class="flex-none">
-      <ul class="menu menu-horizontal p-0">
-        <li>
-          <router-link to="/">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/not-found">NotFound</router-link>
-        </li>
-        <li>
-          <router-link :to="`/hi/${counter.count}`" replace>DynamicRoute:{{ counter.count }}</router-link>
-        </li>
-        <li>
-          <router-link to="/about">About</router-link>
-        </li>
-        <li>
-            <LanguageSwitcher />
-        </li>
-      </ul>
-    </div>
+            <ul class="menu menu-horizontal p-0">
+                <li>
+                    <router-link class="text-white" to="/">Home</router-link>
+                </li>
+                <li>
+                    <router-link class="text-white" to="/not-found">NotFound</router-link>
+                </li>
+                <li>
+                    <router-link class="text-white" :to="`/hi/${counter.count}`" replace>DynamicRoute:{{ counter.count }}</router-link>
+                </li>
+                <li>
+                    <router-link class="text-white" to="/about">About</router-link>
+                </li>
+                <li>
+                    <LanguageSwitcher />
+                </li>
+            </ul>
+        </div>
         <div class="flex-1">
-      <slot></slot>
-    </div>
+        <slot></slot>
+        </div>
     </header>
 </template>
 
